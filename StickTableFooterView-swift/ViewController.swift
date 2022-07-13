fe6ed0a49138bfac12f5f0f4ebca5534bd7c1cae
Phil Chang <phil.chang@yahooinc.com>
2022-07-13 21:26:14 +0800

    Initial Commit

---
+++ StickTableFooterView-swift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StickTableFooterView-swift
+//
+//  Created by Phil Chang on 2022/7/13.
+//  Copyright © 2022 Yahoo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
